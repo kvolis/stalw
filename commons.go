31fe49f94c926c8094a902869775af66f4fc2a89
aakislov <kislov.andrew@gmail.com>
2024-02-16 23:56:37 +0300

    MostFrequent added to comparable types

--- commons.go
+++ commons.go
@@ -10,3 +10,34 @@ func Count[T comparable](element T, elements []T) int {
 	}
 	return cnt
 }
+
+// MostFrequent returns a list of the single most frequently occurring element,
+// or a list of such elements if the number of occurrences matches.
+// If input len equal zero, returns nil
+func MostFrequent[T comparable](elements []T) []T {
+	ln := len(elements)
+	if ln == 0 {
+		return nil
+	}
+
+	maxCnt := 0
+	stats := make(map[T]int)
+
+	for _, num := range elements {
+		stats[num]++
+		if n := stats[num]; n > maxCnt {
+			maxCnt = n
+			if maxCnt > ln/2 {
+				break
+			}
+		}
+	}
+
+	res := []T{}
+	for num, cnt := range stats {
+		if cnt == maxCnt {
+			res = append(res, num)
+		}
+	}
+	return res
+}
