c5744362b326ab341d12c04a615af4c60cde5961
aakislov <kislov.andrew@gmail.com>
2024-03-01 12:21:37 +0300

    CyrToLat completed

--- strconv.go
+++ strconv.go
@@ -7,5 +7,41 @@ package stalw
 // For example, 'йцукенг' ('ЙЦУКЕНГ') will turn into 'йцykehг' ('ЙЦYKEHГ'),
 // where 'у', 'к', 'е', 'н' ('У', 'К', 'Е', 'Н') have become Latin 'y', 'k', 'e', 'h' ('Y', 'K', 'E', 'H').
 func CyrToLat(input string) string {
+	var cyrToLat = map[rune]rune{
+		'а': 'a',
+		'в': 'b',
+		'е': 'e',
+		'к': 'k',
+		'м': 'm',
+		'н': 'h',
+		'о': 'o',
+		'р': 'p',
+		'с': 'c',
+		'т': 't',
+		'у': 'y',
+		'х': 'x',
+	}
 
+	runes := []rune(input)
+
+	for i, r := range runes {
+		var isCap bool
+		symb := r
+
+		if r >= 'А' && r <= 'Я' {
+			r += -'А' + 'а'
+			isCap = true
+		}
+
+		if v, ok := cyrToLat[r]; ok {
+			symb = v
+			if isCap {
+				symb += -'а' + 'А'
+			}
+		}
+
+		runes[i] = symb
+	}
+
+	return string(runes)
 }
